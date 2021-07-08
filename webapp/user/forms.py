fbf7f5793a3fa10ecd5bf0525c32b7aeb2a9fd8c
Serg Nikolaev <kndnou@yandex.ru>
2021-07-08 21:36:59 +1100

    добавил отображение ошибок при регистрации

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from webapp.user.models import User
 
 
 class LoginForm(FlaskForm):
@@ -47,6 +49,21 @@ class RegistrationForm(FlaskForm):
         render_kw={"class": "form-control"}
     )
     submit = SubmitField(
-        'Войти',
+        'Зарегистрироваться',
         render_kw={"class": "btn btn-primary"}
     )
+
+    def validate_username(self, username):
+        user_count = User.query.filter_by(username=username.data).count()
+        print(user_count)
+        print(bool(user_count))
+        if user_count > 0:
+            raise ValidationError(
+                'Пользователь с таким именем уже зарегистрирован')
+
+    def validate_email(self, email):
+        email_count = User.query.filter_by(email=email.data).count()
+        print(email_count)
+        print(bool(email_count))
+        if email_count > 0:
+            raise ValidationError('Такая почта уже используется')
